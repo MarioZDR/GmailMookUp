35d84ead7f5b4407622f6d9e0d9c5d498a227ace
MarioZDR <125427714+MarioZDR@users.noreply.github.com>
2024-02-03 18:33:09 -0700

    Mook Up

---
+++ GmailMookUp_233259_233242/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GmailMookUp_233259_233242"
+include(":app")
